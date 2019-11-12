98d9193580338b1b4150900025e624e25d090a05
Yenny <melchorj2@gmail.com>
2019-11-11 18:41:06 -0600

    Birthday

--- p5/Birthday/sketch.js
+++ p5/Birthday/sketch.js
@@ -6,8 +6,22 @@ var image1;
 function setup() {
   // put setup code here
   createCanvas(windowWidth, windowHeight);
+  image0 = loadImage("Assets/Sanka.png")
   image1 = loadImage("Assets/SlimeyArt.jpg")
   image2 = loadImage("Assets/Penlink.png")
+  image3 = loadImage("Assets/Lusmiasm.jpg")
+  image4 = loadImage("Assets/Red.png")
+  image5 = loadImage("Assets/Harpy.png")
+  image6 = loadImage("Assets/Remake.jpg")
+  image7 = loadImage("Assets/SweetShop1.png")
+  image8 = loadImage("Assets/Neil.png")
+  image9 = loadImage("Assets/SatyrSatyr.png")
+  image10 = loadImage("Assets/Ludi.png")
+  image11 = loadImage("Assets/Tazwomante.png")
+  image12 = loadImage("Assets/NeoHajime.png")
+  image13 = loadImage("Assets/Oddman.jpg")
+  image14 = loadImage("Assets/EpicPotato.png")
+  image15 = loadImage("Assets/Opaquely.png")
 }
 
 function draw() {
@@ -16,17 +30,118 @@ function draw() {
   switch(myState){
     case 0:
     background(255, 255, 255);
-    text("SlimeyArt", 100, 100);
+    text("Sanka", 100, 100);
+    text("HAPPY BIRTHDAY JACKIE!!!", 500, 100)
     textSize(50);
-    image(image1, 400, 100);
+    image(image0, 470, 100);
     break;
 
     case 1:
     background(255, 255, 255);
+    text("SlimeyArt", 100, 100);
+    textSize(50);
+    image(image1, 430, 50);
+    break;
+
+    case 2:
+    background(255, 255, 255);
     text("Penlink", 100, 100);
     textSize(50);
-    image(image2, 430, 50);
+    image(image2, 400, 50);
     break;
+
+    case 3:
+    background(255, 255, 255);
+    text("Lusmiasm", 100, 100);
+    textSize(50);
+    image(image3, 430, 50);
+    break;
+
+    case 4:
+    background(255, 255, 255);
+    text("Red", 100, 100);
+    textSize(50);
+    image(image4, 430, 50);
+    break;
+
+    case 5:
+    background(255, 255, 255);
+    text("Harpy", 100, 100);
+    textSize(50);
+    image(image5, 430, 50);
+    break;
+
+    case 6:
+    background(255, 255, 255);
+    text("Remake", 100, 100);
+    textSize(50);
+    image(image6, 430, 50);
+    break;
+
+    case 7:
+    background(255, 255, 255);
+    text("Sweetshop", 100, 100);
+    textSize(50);
+    image(image7, 400, 50);
+    break;
+
+    case 8:
+    background(255, 255, 255);
+    text("Neil", 100, 100);
+    textSize(50);
+    image(image8, 430, 50);
+    break;
+
+    case 9:
+    background(255, 255, 255);
+    text("SatyrSatyr", 100, 100);
+    textSize(50);
+    image(image9, 430, 50);
+    break;
+
+    case 10:
+    background(255, 255, 255);
+    text("Ludi", 100, 100);
+    textSize(50);
+    image(image10, 430, 50);
+    break;
+
+    case 11:
+    background(255, 255, 255);
+    text("Tazwomante", 100, 100);
+    textSize(50);
+    image(image11, 430, 50);
+    break;
+
+    case 12:
+    background(255, 255, 255);
+    text("NeoHajime", 100, 100);
+    textSize(50);
+    image(image12, 430, 50);
+    break;
+
+    case 13:
+    background(255, 255, 255);
+    text("Oddman", 100, 100);
+    textSize(50);
+    image(image13, 430, 50);
+    break;
+
+    case 14:
+    background(255, 255, 255);
+    text("EpicPotato", 100, 100);
+    textSize(50);
+    image(image14, 430, 50);
+    break;
+
+    case 15:
+    background(255, 255, 255);
+    text("OpaquelyTranslucent", 100, 100);
+    textSize(50);
+    image(image15, 500, 100);
+    break;
+
+
   }
 
 }
@@ -34,7 +149,7 @@ function draw() {
 function mouseReleased(){
   myState++;
 
-  if (myState > 1) {
+  if (myState > 15) {
     myState = 0;
   }
 }
